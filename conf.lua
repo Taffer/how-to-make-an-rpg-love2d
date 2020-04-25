f0471325ae7441075ae9202dd06a9444b3e9f2cc
Taffer <chrish@pobox.com>
2020-04-25 08:41:22 -0400

    Use screen size and tile size to calculate number of tiles.

--- conf.lua
+++ conf.lua
@@ -5,7 +5,7 @@
 
 function love.conf(t)
     t.window.title = "How To Make An RPG"
-    t.window.width = 740
+    t.window.width = 736 -- Was 740 originally, but that leaves a fractional tile.
     t.window.height = 480
 
     t.identity = "How To Make An RPG"
--- main.lua
+++ main.lua
@@ -5,7 +5,9 @@
 
 -- Horrible, horrible globals.
 gameState = {
-    grass = nil
+    grass = nil,
+    tilesPerRow = 0,
+    tilesPerColumn = 0
 }
 
 -- Love callbacks.
@@ -14,11 +16,26 @@ function love.load()
 
     love.graphics.setDefaultFilter('nearest', 'nearest')
     gameState.grass = love.graphics.newImage('assets/grass_tile.png')
+
+    -- Calculate the number of tiles we can draw in a row based on the tile
+    -- size.
+    gameState.tilesPerRow = math.floor(love.graphics.getWidth() / gameState.grass:getWidth())
+    gameState.tilesPerColumn = math.floor(love.graphics.getHeight() / gameState.grass:getHeight())
 end
 
 function love.draw()
     love.graphics.setColor(1, 1, 1, 1)
     love.graphics.draw(gameState.grass)
+
+    local y = 32
+    local x = 32
+    love.graphics.print('Display width: ' .. love.graphics.getWidth(), x, y)
+    y = y + 16
+    love.graphics.print('Display height: ' .. love.graphics.getHeight(), x, y)
+    y = y + 16
+    love.graphics.print('Tiles per row: ' .. gameState.tilesPerRow, x, y)
+    y = y + 16
+    love.graphics.print('Tiles per column: ' .. gameState.tilesPerColumn, x, y)
 end
 
 -- Event generation.
